5e5d5713b0aaee1484271f256312d162916c1298
Bhejjaa <thapapujendra1@gmail.com>
2024-05-17 10:56:14 +0545

    skeletal structure along with assets set up

---
+++ android/app/src/main/kotlin/com/pujendra/vendor_vault/vendor_vault/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pujendra.vendor_vault.vendor_vault
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:vendor_vault/app/app.dart';
+
+void main(){
+  runApp(
+    const App(),
+  );
+}
