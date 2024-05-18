a8aaa9bad8803f60c316472ada02e972ae54545c
Bhejjaa <thapapujendra1@gmail.com>
2024-05-18 19:31:26 +0545

    custom text field created

---
+++ lib/common/custom_text_field.dart
@@ -0,0 +1,52 @@
+// lib/app/common/custom_text_field.dart
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final bool isPassword;
+  final Function(String)? onChanged;
+  final Color textColor; // Added textColor parameter
+  final Color borderColor;
+  final Color fillColor; // Added borderColor parameter
+
+  CustomTextField({
+    required this.labelText,
+    this.isPassword = false,
+    this.onChanged,
+    required this.textColor, // Set a default value for textColor
+    required this.borderColor, // Set a default value for borderColor
+    required this.fillColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          '$labelText ',
+          style: TextStyle(
+            color: Colors.grey.shade600,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(width: 8),
+        Expanded(
+          child: TextFormField(
+            obscureText: isPassword,
+            style: TextStyle(color: textColor),
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: borderColor),
+              ),
+              fillColor: fillColor,
+              filled: true,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
