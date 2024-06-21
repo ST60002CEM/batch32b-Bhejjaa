5fa735c72666b54b0b018f7d4286750cb88b0d2c
Bhejjaa <thapapujendra1@gmail.com>
2024-06-21 22:20:11 +0545

    shared preferences file  added

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -32,3 +32,98 @@ class AuthRemoteDataSource {
           "account type": user.accountType
         },
       );
+       if (response.statusCode == 200) {
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+  Future<Either<Failure, bool>> loginUser(
+    String username,
+    String password,
+  ) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.login,
+        data: {
+          "username": username,
+          "password": password,
+        },
+      );
+      if (response.statusCode == 200) {
+        String token = response.data["token"];
+        // Save token to shared prefs
+        await userSharedPrefs.setUserToken(token);
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      // Get the token from shared prefs
+      String? token;
+      var data = await userSharedPrefs.getUserToken();
+      data.fold(
+            (l) => token = null,
+            (r) => token = r!,
+      );
+
+      var response = await dio.get(
+        ApiEndpoints.currentUser,
+        options: Options(headers: {
+          'Authorization': 'Bearer $token',
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        GetCurrentUserDto getCurrentUserDto = GetCurrentUserDto.fromJson(response.data);
+
+        return Right(getCurrentUserDto.toEntity());
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+}
+
+
