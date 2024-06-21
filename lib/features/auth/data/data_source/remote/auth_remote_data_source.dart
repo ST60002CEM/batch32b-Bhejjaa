f908b612a6290aa07e8239e4d1cd956265a53aaf
Bhejjaa <thapapujendra1@gmail.com>
2024-06-21 22:12:24 +0545

    apis  added

--- lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -1,5 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vendor_vault/app/constants/api_endpoints.dart';
+import 'package:vendor_vault/core/error/failure.dart';
 import 'package:vendor_vault/core/networking/remote/http_service.dart';
+import 'package:vendor_vault/features/auth/domain/entity/auth_entity.dart';
 
 final authRemoteDataSourceProvider = Provider(
   (ref) => AuthRemoteDataSource(
@@ -8,3 +13,22 @@ final authRemoteDataSourceProvider = Provider(
   ),
 );
 
+class AuthRemoteDataSource {
+  final Dio dio;
+  final UserSharedPrefs userSharedPrefs;
+
+  AuthRemoteDataSource({required this.dio, required this.userSharedPrefs});
+
+  Future<Either<Failure, bool>> registerUser(AuthEntity user) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.register,
+        data: {
+          "fname": user.fname,
+          "lname": user.lname,
+          "phone": user.phone,
+          "username": user.username,
+          "password": user.password,
+          "account type": user.accountType
+        },
+      );
